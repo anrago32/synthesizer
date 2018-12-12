b833930d6bc34536186e35373f649d4fa1871591
Alex Rago <anrago32@gmail.com>
2018-12-11 22:17:14 -0500

    Update source

--- source/audio-generation.hs
+++ source/audio-generation.hs
@@ -7,4 +7,4 @@ generateAudio pitch time = sin period * amplitude
   where amplitude = 10
         frequency = scientificPitch pitch
         period = frequency * fromIntegral time * pi * 2 / sampleRate
-        sampleRate = 48000.0
+        sampleRate = 48000
--- source/program.hs
+++ source/program.hs
@@ -1,11 +1,12 @@
 module Main where
 
+import Sound.PortAudio
 import AudioGeneration
 import Pitch
 
 main :: IO ()
 main = do
   let tone = generateAudio (C, 0)
-  let times = [t | t <- [0..3000], t `mod` 750 == 0]
+  let times = [t | t <- [0..3000], t `mod` 375 == 0]
   let wave = map tone times
   mapM_ print wave
