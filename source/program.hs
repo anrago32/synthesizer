859bdc6bc4cec513168582c7eacbaa17fd084d4e
Alex Rago <anrago32@gmail.com>
2018-12-13 12:28:01 -0500

    Update documentation

--- source/program.hs
+++ source/program.hs
@@ -1,17 +1,44 @@
 module Main where
 
+import Graphics.UI.Gtk
 import Sound.Pulse.Simple
 import PitchConversion
 import ToneGeneration
 
 main :: IO ()
 main = do
+  initGUI
+  window <- windowNew
+  set window
+    [ windowTitle := "Program"
+    , containerBorderWidth := 20
+    ]
+
+  button <- buttonNew
+  onClicked button playChord
+  box <- labelBox "Play Chord"
+  containerAdd button box
+  containerAdd window button
+
+  widgetShowAll window
+  onDestroy window mainQuit
+  mainGUI
+
+labelBox :: String -> IO HBox
+labelBox txt = do
+  box   <- hBoxNew False 0
+  set box [containerBorderWidth := 2]
+  label <- labelNew (Just txt)
+  boxPackStart box label PackNatural 3
+  return box
+
+playChord = do
   let sampleFormat = F32 LittleEndian
   let sampleSpec = SampleSpec sampleFormat 48000 1
   s <- simpleNew Nothing "nothing"
     Play Nothing "nothing"
     sampleSpec Nothing Nothing
-  let times = [0..48000 * 4]
+  let times = [0..24000]
   let tone1 = generateTone (C, 5) times
   let tone2 = generateTone (E, 5) times
   let tone3 = generateTone (G, 5) times
