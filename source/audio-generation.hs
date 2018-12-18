41f24f2773f691fb52a8e6176127ba53464bdbc3
Alex Rago <anrago32@gmail.com>
2018-12-18 12:49:07 -0500

    Update source

--- source/audio-generation.hs
+++ source/audio-generation.hs
@@ -2,24 +2,28 @@ module AudioGeneration where
 
 import PitchConversion
 
+{-
 cMajor :: Int -> Float
 cMajor time = generateChord [tone1, tone2, tone3]
-  where tone1 = generateTone (C, 5) time
-        tone2 = generateTone (E, 5) time
-        tone3 = generateTone (G, 5) time
+  where tone1 = sineOscillator (C, 5) time
+        tone2 = sineOscillator (E, 5) time
+        tone3 = sineOscillator (G, 5) time
 
 fMajor :: Int -> Float
 fMajor time = generateChord [tone1, tone2, tone3]
-  where tone1 = generateTone (F, 5) time
-        tone2 = generateTone (A, 5) time
-        tone3 = generateTone (C, 6) time
+  where tone1 = sineOscillator (F, 5) time
+        tone2 = sineOscillator (A, 5) time
+        tone3 = sineOscillator (C, 6) time
 
 generateChord :: [Float] -> Float
 generateChord [tone] = tone
 generateChord (tone:tones) = (tone + generateChord tones) / 2.0
+-}
 
-generateTone :: Pitch -> Int -> Float
-generateTone pitch time = sin period
-  where frequency = standardPitch pitch
-        period = fromIntegral time * frequency * pi * 2.0 / sampleRate
+sineOscillator :: Float -> Int -> Float
+sineOscillator frequency time = sin phase
+  where phase = fromIntegral time * frequency * pi * 2.0 / sampleRate
         sampleRate = 48000.0
+
+finalOutput :: Int -> Float
+finalOutput time = sin (sineOscillator 440.0 time * 2)
--- source/program.hs
+++ source/program.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Data.Text (pack)
 import Graphics.UI.Gtk
 import Sound.Pulse.Simple
 import AudioGeneration
+import PitchConversion
 
 main :: IO ()
 main = do
@@ -10,25 +12,117 @@ main = do
   window <- windowNew
   set window
     [ windowTitle := "Synthesizer"
-    , containerBorderWidth := 20
+    , windowResizable := False
     ]
 
-  button <- buttonNew
-  button `onClicked` playChords
-  box <- labelBox "Play Chords"
-  button `containerAdd` box
-  window `containerAdd` button
+  widgetSetSizeRequest window 600 400
+  table <- tableNew 8 64 False
+  containerAdd window table
+
+  -- First Row
+  row1 <- hBoxNew True 10
+  containerSetBorderWidth row1 10
+  tableAttachDefaults table row1 0 8 0 1
+
+  button1 <- buttonNew
+  containerAdd row1 button1
+  button2 <- buttonNew
+  containerAdd row1 button2
+  button3 <- buttonNew
+  containerAdd row1 button3
+  button4 <- buttonNew
+  containerAdd row1 button4
+
+  -- Second Row
+  row2 <- hBoxNew True 10
+  containerSetBorderWidth row2 10
+  tableAttachDefaults table row2 0 8 1 32
+
+  row2Section1 <- hBoxNew True 0
+  containerAdd row2 row2Section1
+  row2Section2 <- hBoxNew True 0
+  containerAdd row2 row2Section2
+  row2Section3 <- hBoxNew True 0
+  containerAdd row2 row2Section3
+  row2Section4 <- hBoxNew True 0
+  containerAdd row2 row2Section4
+
+  volumeSlider <- createSlider
+  containerAdd row2Section1 volumeSlider
+  pitchSlider <- createSlider
+  containerAdd row2Section1 pitchSlider
+  portamentoSlider <- createSlider
+  containerAdd row2Section2 portamentoSlider
+  envSlider1 <- createSlider
+  containerAdd row2Section2 envSlider1
+  envSlider2 <- createSlider
+  containerAdd row2Section3 envSlider2
+  envSlider3 <- createSlider
+  containerAdd row2Section3 envSlider3
+  envSlider4 <- createSlider
+  containerAdd row2Section4 envSlider4
+  envSlider5 <- createSlider
+  containerAdd row2Section4 envSlider5
+
+  -- Third Row
+  row3 <- hBoxNew True 10
+  containerSetBorderWidth row3 10
+  tableAttachDefaults table row3 0 8 32 33
+
+  oscSelector <- comboBoxNewText
+  comboBoxAppendText oscSelector (pack "Frequency Mod")
+  containerAdd row3 oscSelector
+  lfoSelector <- comboBoxNewText
+  comboBoxAppendText lfoSelector (pack "None")
+  containerAdd row3 lfoSelector
+  effSelector <- comboBoxNewText
+  comboBoxAppendText effSelector (pack "None")
+  containerAdd row3 effSelector
+  filSelector <- comboBoxNewText
+  comboBoxAppendText filSelector (pack "None")
+  containerAdd row3 filSelector
+
+  -- Fourth Row
+  row4 <- hBoxNew True 10
+  containerSetBorderWidth row4 10
+  tableAttachDefaults table row4 0 8 33 64
+
+  row4Section1 <- hBoxNew True 0
+  containerAdd row4 row4Section1
+  row4Section2 <- hBoxNew True 0
+  containerAdd row4 row4Section2
+  row4Section3 <- hBoxNew True 0
+  containerAdd row4 row4Section3
+  row4Section4 <- hBoxNew True 0
+  containerAdd row4 row4Section4
+
+  oscSlider1 <- createSlider
+  containerAdd row4Section1 oscSlider1
+  oscSlider2 <- createSlider
+  containerAdd row4Section1 oscSlider2
+  lfoSlider1 <- createSlider
+  containerAdd row4Section2 lfoSlider1
+  lfoSlider2 <- createSlider
+  containerAdd row4Section2 lfoSlider2
+  effSlider1 <- createSlider
+  containerAdd row4Section3 effSlider1
+  effSlider2 <- createSlider
+  containerAdd row4Section3 effSlider2
+  filSlider1 <- createSlider
+  containerAdd row4Section4 filSlider1
+  filSlider2 <- createSlider
+  containerAdd row4Section4 filSlider2
+
   widgetShowAll window
-  window `onDestroy` mainQuit
+  onDestroy window mainQuit
   mainGUI
 
-labelBox :: String -> IO HBox
-labelBox text = do
-  box <- hBoxNew False 0
-  set box [containerBorderWidth := 2]
-  label <- labelNew (Just text)
-  boxPackStart box label PackNatural 3
-  return box
+createSlider :: IO VScale
+createSlider = do
+  slider <- vScaleNewWithRange 0 99 1
+  rangeSetInverted slider True
+  scaleSetDrawValue slider False
+  return slider
 
 playAudio :: [Float] -> IO ()
 playAudio audio = do
@@ -39,10 +133,8 @@ playAudio audio = do
   simpleDrain s
   simpleFree s
 
-playChords :: IO ()
-playChords = do
-  let times = [0..24000]
-  let audio1 = map cMajor times
-  let audio2 = map fMajor times
-  let audio = audio1 ++ audio2 ++ audio1
+playNote :: IO ()
+playNote = do
+  let times = [0..48000 * 2]
+  let audio = map finalOutput times
   playAudio audio
